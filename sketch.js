a8d76bb01ed499f8916647a43e27c72522d9936c
asukabaka <48495833+asukabaka@users.noreply.github.com>
2020-02-26 22:36:30 -0800

    randomizer

--- sketch.js
+++ sketch.js
@@ -1,6 +1,14 @@
-//favorite foods
 
-let foods = ["bibimbap", "ramen", "hotcheetos", "kimchi"]
+let foods = [{
+	name:"bibimbap",
+}, {
+
+name: "ramen"
+},{
+	name: "hotcheetos"
+},{
+	name: "kimchi"
+}];
 
 function setup() {
 	createCanvas(600,600);
@@ -11,4 +19,14 @@ function setup() {
 
 function draw() {
 
+}
+
+function mousePressed(){
+	if (foods[0]){
+		background(random(200, 255));
+		randomIndex = int(random(foods.length));
+		text(foods[randomIndex].name, 50, 50);
+		foods.splice(randomIndex, 1);
+
+	}
 }
