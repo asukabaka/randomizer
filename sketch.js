0a8973bf5fba8fb0c2dfef5260dd144452351d40
asukabaka <48495833+asukabaka@users.noreply.github.com>
2020-03-02 20:33:13 -0800

    randomizer assignment

--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
 
 let foods = [{
 	name:"bibimbap",
+	topping:"seaweed"
 }, {
 
-name: "ramen"
+name: "ramen",
+topping: "greenonions"
 },{
-	name: "hotcheetos"
+	name: "seafoodpancake",
+	topping: "moreseafood"
 },{
-	name: "kimchi"
+	name: "kimchi",
+	topping: "seasemeseeds"
 }];
+
+let randomIndex;
+let animating = false;
 
 function setup() {
 	createCanvas(600,600);
@@ -19,14 +26,29 @@ function setup() {
 
 function draw() {
 
+	if(animating == true){
+		ellipse(random(width), random(height), random(50, 200));
+	}
+
 }
 
-function mousePressed(){
+function randomizer(){
+
+	animating = false;
+
+
 	if (foods[0]){
 		background(random(200, 255));
 		randomIndex = int(random(foods.length));
-		text(foods[randomIndex].name, 50, 50);
+		text(`${foods[randomIndex].name}'s favorite topping inspect ${foods[randomIndex].topping}`, 50, 50);
 		foods.splice(randomIndex, 1);
-
+	} else {
+		background(random(200,255));
+		text("nothing left!", 50, 50);
 	}
+}
+
+function mousePressed(){
+	animating = true;
+	setTimeout(randomizer, 2000);
 }
