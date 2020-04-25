4d49c6ab1b9903c4e99f17dde9e912b274143e02
asukabaka <48495833+asukabaka@users.noreply.github.com>
2020-04-24 23:52:17 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,46 +1,63 @@
 
+
+//create array foods
 let foods = [{
+	//create object
 	name:"bibimbap",
 	topping:"seaweed"
 }, {
-
+//create object
 name: "ramen",
 topping: "green onions"
 },{
+	//create object
 	name: "seafood pancake",
 	topping: "soysauce"
 },{
+	//create object
 	name: "kimchi",
 	topping: "seaseme seeds"
 }];
 
+//create variable randomIndex
 let randomIndex;
+//creat variable animating = false so the animation starts at false state
 let animating = false;
+//create foodpics array
 let foodpics = [];
+//image counter will start at zero
 let imageCounter = 0;
+//create variable button
 let button;
 
 
 function preload(){
+	//preload food pictures from 0 to 3
 	for (let i = 0; i <= 3; i++){
+		//food picture naming schema loaded into foodpics array
 		foodpics[i] = loadImage(`images/food_${i}.jpg`)
 	}
 }
 
 function setup() {
+	// standard setup
 	createCanvas(windowWidth*0.7,windowHeight*0.7);
 	background('rgba(255,255,255,.2)');
 	textSize(32);
 	textAlign(CENTER);
 	imageMode(CENTER);
 	frameRate(8);
+	//console logging images being loaded
 	console.log(foods[3]);
 	console.log(foodpics);
 
+//create text centered in the middle of the screen
 	text("explore different Korean foods :-)", width / 2, height / 2);
-
+//create button
 	 button = createButton("Try a new dish ~");
+	 //button with function buttonPressed
 	 button.mousePressed(buttonPressed);
+	 //
 	 button.class("randomizerButton");
 }
 
@@ -61,7 +78,15 @@ function randomizer(){
 
 	animating = false;
 	if (foods[0]) {
-		// clear();
+		clear();
+		if(animating == true){
+		clear();
+		image(foodpics[imageCounter], width/2, height/2);
+		if (imageCounter < foodpics.length - 1){
+		imageCounter++;} else {
+			imageCounter = 0;
+		}
+	}
 
 	}
 
